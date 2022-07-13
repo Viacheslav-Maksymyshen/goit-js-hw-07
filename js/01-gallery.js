9f9f5e5bb11f2434e6008a32c1c4b8f376f0f60b
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-07-13 21:19:25 +0300

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+// gallery.setAttribute(
+//   "style",
+//   "list-style-type: none; display: flex; align-items: center; justify-content: space-evenly"
+// );
+const makeImagesItems = ({ preview, description }) =>
+  `<li class = "gallery__items"><img class = "gallery__items-img"  src = "${preview}" alt = "${description}"></li>`;
+const imagesItems = galleryItems.map(makeImagesItems).join("");
+gallery.insertAdjacentHTML("afterbegin", imagesItems);
+console.log(imagesItems);
 
 console.log(galleryItems);
