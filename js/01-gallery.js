91e162d2ae00d5154414f537d4e35594d59cef75
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2022-07-15 15:41:41 +0300

    render

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,17 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const gallery = document.querySelector(".gallery");
-// gallery.setAttribute(
-//   "style",
-//   "list-style-type: none; display: flex; align-items: center; justify-content: space-evenly"
-// );
-const makeImagesItems = ({ preview, description }) =>
-  `<li class = "gallery__items"><img class = "gallery__items-img"  src = "${preview}" alt = "${description}"></li>`;
+
+const makeImagesItems = ({ preview, description, original }) =>
+  `<li class = "gallery__item">
+  <img class = "gallery__image"  data-set = "${original}" src = "${preview}" alt = "${description}" ></li>`;
 const imagesItems = galleryItems.map(makeImagesItems).join("");
 gallery.insertAdjacentHTML("afterbegin", imagesItems);
-console.log(imagesItems);
 
-console.log(galleryItems);
+gallery.addEventListener("click", clickItemGallery);
+function clickItemGallery(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  console.log(event.target);
+}
